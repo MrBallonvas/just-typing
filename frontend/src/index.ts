c3ea122be1df30932dc2c49b82cbde19a1bfadf2
MrBallonvas <58231918+MrBallonvas@users.noreply.github.com>
2025-04-06 10:25:41 +0000

    [create-pull-request] automated change

--- frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import * as router from "./router";
-
-// listereners for render content for current route
-window.addEventListener("popstate", router.renderRouteHandler);
-document.addEventListener("DOMContentLoaded", router.renderRouteHandler);
-
-// custom handler for links
-document.addEventListener("click", router.linkListenerHandler);
--- frontend/src/views/Stats/index.ts
+++
@@ -1,1 +0,0 @@
-export default "<h1>Stats</h1>";

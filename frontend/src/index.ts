3cd67271cb988378bb7d5b88fb400bf87afb2794
MrBallonvas <vasilisrusgame@gmail.com>
2025-04-27 01:14:02 +0500

    turn off serviceWorker

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -8,5 +8,9 @@ import routerSetup from "./router";
 //     });
 //   });
 // }
-
+navigator.serviceWorker.getRegistrations().then((registrations) => {
+  for (const registration of registrations) {
+    registration.unregister();
+  }
+});
 routerSetup();
